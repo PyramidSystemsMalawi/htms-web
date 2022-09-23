0c1889e9a092819e5141f568980bc48a95f0c93f
Clifford Mwale <cliffmwale97@gmail.com>
2022-09-23 05:57:28 +0200

    Commit

---
+++ database/seeders/TIPChecklistSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TIPChecklistSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Read all tip questions from json file in public folder
+        $json = json_decode(file_get_contents(public_path('qualifiers.json')),true);
+
+        foreach($json AS $q){
+            DB::table('qualifiers')->insert([
+                'question' => $q->question,
+                'responseType'=>$q->responseType,
+                'nullable'=>$q->nullable,
+                'possible_answers'=>$q->possible_answers,
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+
+    }
+}
