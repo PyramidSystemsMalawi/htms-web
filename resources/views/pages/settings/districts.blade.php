372f4b7ddce54c8dc44aa164dfb155398ba18ea7
Clifford Mwale <cliffmwale97@gmail.com>
2022-09-29 01:14:40 +0200

    Commit

--- resources/views/pages/settings/districts.blade.php
+++ resources/views/pages/settings/districts.blade.php
@@ -32,7 +32,7 @@
             <tbody>
                 @foreach($districts AS $k3y => $district)
                     <tr>
-                        <td>{{$loop->iteration+1}}</td>
+                        <td>{{$loop->iteration}}</td>
                         <td>{{$district->district_name}}</td>
                         <td>{{$district->district_code}}</td>
                     </tr>
