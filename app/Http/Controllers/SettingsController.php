9bf712d8f11d7b00a8509185c80be1f5e1cba219
Clifford Mwale <cliffmwale97@gmail.com>
2022-09-29 01:11:32 +0200

    Commit

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -17,7 +17,7 @@ class SettingsController extends Controller
      */
     public function index()
     {
-        $districts = District::orderBy('district_name','DESC')->all();
+        $districts = District::orderBy('district_name','DESC')->get();
         $userdata = Auth::user();
         return view('pages.settings.districts')->with([
             'title' => 'Districts',
